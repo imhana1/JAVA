9c8b9a2106a4bfa92d73da1d5aacfc7072411ec9
im-hana <qha_nap@nate.com>
2025-04-02 11:26:33 +0900

    todo 완성

--- demo3/src/main/java/com/example/demo3/todo/Todo.java
+++ demo3/src/main/java/com/example/demo3/todo/Todo.java
@@ -2,15 +2,20 @@ package com.example.demo3.todo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Getter
-@AllArgsConstructor
 public class Todo {
     private int tno;
     private String title;
     private final LocalDate regDate = LocalDate.now();
-    private final boolean finish = false;
+    @Setter
+    private boolean finish = false;
+    public Todo(int tno, String title) {
+        this.tno = tno;
+        this.title = title;
+    }
 }
--- demo3/src/main/java/com/example/demo3/todo/TodoController.java
+++ demo3/src/main/java/com/example/demo3/todo/TodoController.java
@@ -15,11 +15,6 @@ public class TodoController {
     private List<Todo> todos = new ArrayList<>();
     private int tno = 1;
 
-    // C - GET, POST
-    @GetMapping("/todo/write")
-    public void write() {
-
-    }
     // <input nate='title'>이 요청 객체에 담겨 백엔드로 전달되면 같은 이름의 파라미터에 담는다
     // @RequestParam : 요청 객체에서 이름이 같은 데이터를 추출해 파라미터에 담아라
     //                 스프링은 그 어떤 데이터라도 파라미터로 변환할 수 있을까?
@@ -37,4 +32,54 @@ public class TodoController {
         return new ModelAndView("/todo/list").addObject("todos", todos);
     }
 
+    @PostMapping("/todo/finish")
+    public ModelAndView finish(@RequestParam int tno) {
+        for(Todo todo:todos) {
+            if (todo.getTno() == tno) {
+                todo.setFinish(true);
+                break;
+            }
+        }
+        return new ModelAndView("redirect:/todo/list");
+    }
+
+    @PostMapping("/todo/delete")
+    public ModelAndView delete(@RequestParam int tno) {
+        System.out.println(tno);
+        // 0번째 원소부터 차례대로 찾으면서 삭제할 경우, 접근하지 않고 통과하는 원소가 있다
+        // 인덱스 : 0 1 2 3
+        // 값      A B C D
+        // B 를 찾아서 삭제할 경우 -> B가 지워지는 순간에 C가 첫번째, D가 두번째 원소가 된다
+        //                      -> 인덱스는 1에서 2로 증가 -> C 를 건너 뛰고 D 로 접근 -> 자바가 용납하지 않는다
+        // 자바는 list 에서 원소를 찾아서 삭제 할때는 거꾸로 돌려서 찾는다
+        for(int i = todos.size() -1; i >= 0; i --) {
+            if (todos.get(i).getTno() == tno) {
+                todos.remove(i);
+                break;
+            }
+        }
+        return new ModelAndView("redirect:/todo/list");
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
