0d28deba0b56dce0ba1c70c9cc360f2b2b74b53a
imhana1 <ksui3047@gmail.com>
2025-04-08 16:16:36 +0900

    mybatis5

---
+++ mybatis5/src/main/java/com/example/mybatis5/Mybatis5Application.java
@@ -0,0 +1,13 @@
+package com.example.mybatis5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatis5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatis5Application.class, args);
+    }
+
+}
---
+++ mybatis5/src/test/java/com/example/mybatis5/Mybatis5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatis5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatis5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
